6d868b6ef2c4cea0454d527d1f85d9d598557657
MohsenKhashei <m.khasheii@gmail.com>
2023-08-15 18:30:02 +0300

    update service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { EncryptionHelper } from './helper/encryption.helper';
-import { ResponseInitiateKey } from './ResponseInitiateKey';
-import { RequestPublicKeyDto } from './RequestPublicKeyDto';
 import { FileManager } from './helper/fileManager';
 import { UserCredentialDto } from './dto/user.credential.dto';
+import { parse } from 'path';
 import axios from 'axios';
 
 @Injectable()
@@ -13,39 +12,68 @@ export class AppService {
     private readonly fileManager: FileManager,
   ) {}
 
-  callForHandshake(): ResponseInitiateKey {
-    return this.encryptionHelper.initiatorKey();
+  async callForRegister() {
+    try {
+      const result = await this.encryptionHelper.createAllKeys();
+      await this.fileManager.writeFile(JSON.stringify(result));
+      return result;
+    } catch (error) {
+      throw error;
+    }
   }
 
-  async generateSecretKeyFromPublicKey(requestPublicKey: RequestPublicKeyDto) {
-    const secretKey = this.encryptionHelper.initiatorSecret(
-      requestPublicKey.public_key,
+  async callForAuth(userCredentialDto: UserCredentialDto) {
+    //##################################################################
+    //reading data from file
+    const fileData = await this.fileManager.readFile();
+    const parsedData = JSON.parse(fileData);
+    //##################################################################
+    const key = parsedData.key;
+    //##################################################################
+
+    const encryptedData = await this.encryptionHelper.encryptingData(
+      JSON.stringify(userCredentialDto),
+      key,
     );
-    const data = {
-      public_key: requestPublicKey.public_key,
-      secret_key: secretKey,
-    };
-    await this.fileManager.writeFile(JSON.stringify(data));
+    console.log(encryptedData);
+    //##################################################################
+    const url = 'http://localhost:3000/api/auth';
+    const header = { key: parsedData.data.data.publicKey };
+    const dataToSend = { requestType: 'HASH', data: encryptedData };
+    const { data } = await axios.post(url, dataToSend, {
+      headers: header,
+    });
+
     return data;
   }
 
   async sendingEncryptedData(userCredentialDto: UserCredentialDto) {
-    const fileData = await this.fileManager.readFile();
-    const { public_key, secret_key } = JSON.parse(fileData);
-    console.log(public_key);
-    const encryptedPayload = await this.encryptionHelper.encryptPayload(
-      userCredentialDto,
-      secret_key,
-    );
+    // const fileData = await this.fileManager.readFile();
+    // const { public_key, secret_key } = JSON.parse(fileData);
+    // const encryptedPayload = await this.encryptionHelper.encryptPayload(
+    //   JSON.stringify(userCredentialDto),
+    //   secret_key,
+    // );
+    // console.log('encrypted');
+    // console.log(encryptedPayload);
     // const { data } = await axios.post(
-    //   'http://localhost:3000/api/users',
-    //   encryptedPayload,
+    //   'http://localhost:3000/api/auth',
+    //   { data: encryptedPayload },
     //   {
     //     headers: {
-    //       testKey: public_key,
+    //       key: public_key,
     //     },
     //   },
     // );
-    console.log(encryptedPayload);
+    // if (data.responseType == 'HASH') {
+    //   const decryptPayload = await this.encryptionHelper.decryptPayload(
+    //     secret_key,
+    //     data.data,
+    //   );
+    //   return decryptPayload;
+    // }
+    // if (data.responseType == 'JSON') {
+    //   return data.data;
+    // }
   }
 }
